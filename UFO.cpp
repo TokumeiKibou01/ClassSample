a2ea07fc5f7a98f937a371bad6a36ed9bfae0f0d
a <aa@example.com>
2025-12-10 11:25:43 +0900

    【修正】 UFOの動きを修正
    【修正】 UFOの動きを修正
    
    【修正】 UFOの動きを修正
    
    【修正】 UFOの動きを修正

--- UFO.cpp
+++ UFO.cpp
@@ -29,30 +29,26 @@ void UFO::Draw()
 void UFO::RepeatMoveLeftRight()
 {
 	float dt = GetDeltaTime();
-	Vector2D pos = GetPos();
 
 	const float left = 0.0f;
 	const float right = WIN_WIDTH - 20;
 
-	static double a = 0; //Šp“x
-	static int move_y = GetPos().y;
+	static double radius = 0; //Šp“x
+	static int direction = 1; //•ûŒü
 
-	move_y = sin(a * 1.2f) * 10; 
-	SetPos(Vector2D(pos.x + a, pos.y + move_y));
-	static int b = 1; //•„†
-	a += 0.1 * b; //Šp“x‚ð•Ï‚¦‚Ä‚¢‚­
-	if (pos.x > right) {
-		SetPos(Vector2D(right, pos.y));
-		SetVel(Vector2D(-GetVel().x, GetVel().y));
-		move_y = GetPos().y;
-		a = 0;
-		b *= -1;
+	vel_.x = 2 * direction;
+	vel_.y = sin(radius * 1.2f) * 10;
+
+	pos_.x = pos_.x + vel_.x; 
+	pos_.y = pos_.y + vel_.y;
+	
+	radius += 0.1; //Šp“x‚ð•Ï‚¦‚Ä‚¢‚­
+	if (pos_.x > right) {
+		pos_.x = right;
+		direction *= -1;
 	}
-	else if (pos.x < left) {
-		SetPos(Vector2D(left, pos.y));
-		SetVel(Vector2D(-GetVel().x, GetVel().y));
-		move_y = GetPos().y;
-		a = 0;
-		b *= -1;
+	else if (pos_.x < left) {
+		pos_.x = left;
+		direction *= -1;
 	}
 }
